48252eec6c4623eeea24045e6bbdf0eda2dfc50e
Shokhjahon <rustamjanov.shokhjahon@gmail.com>
2026-04-29 07:28:04 -0700

    Upd

--- app/layout.jsx
+++ app/layout.jsx
@@ -8,12 +8,19 @@ const mono = JetBrains_Mono({
 
 export const metadata = {
   title: "sshokh.uz",
-  description: "Special portfolio I crafted for myself to display my work, skills as well as some personal achievements.",
+  description:
+    "Special portfolio I crafted for myself to display my work, skills as well as some personal achievements.",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className={`${mono.className} h-full antialiased dark`}>
+      <head>
+        <meta
+          http-equiv="Content-Security-Policy"
+          content="upgrade-insecure-requests"
+        ></meta>
+      </head>
       <body className="min-h-screen container mx-auto lg:tracking-normal tracking-tighter bg-background text-accent-foreground justify-between">
         {children}
       </body>
